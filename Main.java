5bb6f9b4efc02769f522c267809606052501695b
NagihanKURNAZ <112775522+NagihanKURNAZ@users.noreply.github.com>
2022-12-03 13:04:15 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+        int sayı = 24;
+        
+        if (sayı<24) {
+        	System.out.println("sayı 24 ten küçüktür");
+		}else if (sayı == 24) {
+			System.out.println("sayı 24 e eşittir");
+		}else {
+			System.out.println("sayı 24 ten büyüktür ");
+		}
+        
+       
+	}
+}
